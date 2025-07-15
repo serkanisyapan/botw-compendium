ca6470e3d00b50fd52799cae4455340b72d3557e
serkanisyapan <serkanisyapan1@hotmail.com>
2025-07-15 22:15:41 +0200

    layout changes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import '../main.css'
+    interface CategoryData {
+        category_name: string,
+        category_image: string
+    }
+    let { children } = $props();
+    const category_data:CategoryData[] = [
+        {category_name: "creatures", category_image: "https://botw-compendium.herokuapp.com/api/v3/compendium/entry/horse/image"},
+        {category_name: "equipments", category_image: "https://botw-compendium.herokuapp.com/api/v3/compendium/entry/master_sword/image"},
+        {category_name: "materials", category_image: "https://botw-compendium.herokuapp.com/api/v3/compendium/entry/apple/image"},
+        {category_name: "monsters", category_image: "https://botw-compendium.herokuapp.com/api/v3/compendium/entry/chuchu/image"},
+        {category_name: "treasure", category_image: "https://botw-compendium.herokuapp.com/api/v3/compendium/entry/treasure_chest/image"},
+    ]
+</script>
+
+<div class="layout">
+
+    <aside class="category_sidebar">
+        <a href="/"><p class="sidebar_title">BoTW: Compendium</p></a>
+        <ul class="category_list">
+            {#each category_data as {category_image, category_name}}
+                <a class="category_card" href="/category/{category_name}"><img class="category_image" src="{category_image}" alt="{category_name}"/>{category_name.toUpperCase()}</a>
+            {/each}
+        </ul>
+    </aside>
+
+    <main>
+        {@render children()}
+    </main>
+</div>
+
+<style>
+    .layout {
+        height: 100vh;
+        display: grid;
+        gap: 5em;
+        grid-template-columns: 16em 1fr;
+    }
+    .sidebar_title {
+        margin-left: 40px;
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+    .category_list {
+        display: flex;
+        flex-direction: column;
+        list-style-type: none;
+        gap:15px;
+    }
+    .category_card {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+    }
+    .category_image {
+        border-radius: 50%;
+        width: 40px;
+    }
+    a {
+        text-decoration: none;
+    }
+</style>
--- src/routes/category/[slug]/+page.svelte
+++ src/routes/category/[slug]/+page.svelte
@@ -1,6 +1,53 @@
 <script lang="ts">
-    let { data } = $props();
-    console.log(data.data)
+    interface Data {
+        data: PageData
+    }
+
+    interface PageData {
+        category_data: CategoryData[],
+        current_slug: string
+    }
+
+    interface CategoryData {
+        category: string,
+        common_locations: string[],
+        description: string,
+        image: string,
+        name: string,
+        dlc: string,
+        id: number,
+        drops: string[],
+        edible?: boolean,
+    }
+    let { data }: Data = $props();
 </script>
 
-<h1>This is the specific page for the</h1>
+<div class="category_page">
+    <h1 class="category_title">{data.current_slug.toUpperCase()}</h1>
+    <section class="category_items">
+        {#each data.category_data as {category, common_locations, description, image, name, dlc, id, drops, edible}}
+            <article class="category_item">
+                <img class="category_item_image" src="{image}" alt="{name}"/>
+                <p>{name}</p>
+            </article>
+        {/each}
+    </section>
+</div>
+
+<style>
+    .category_items{
+        display: grid;
+        grid-template-columns: repeat(4, 1fr); 
+        gap: 3em;
+    }
+    .category_item {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: 2px solid black;
+    }
+    .category_item_image {
+        width: 60%;
+    }
+</style>
